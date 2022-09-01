eec75b48e609ac723456efd54ec063c53d74c0b1
Terry Babyuk <tbabyuk@gmail.com>
2022-08-31 23:14:11 -0400

    sales log functionality added and working

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,24 +1,48 @@
 
 // import { booksArray } from '../data/inventoryBooks';
 import {db} from '../firebase/config'
-import {collection, getDocs, updateDoc, doc, increment, query, orderBy} from 'firebase/firestore'
+import {collection, addDoc, getDocs, updateDoc, doc, increment, query, orderBy, serverTimestamp} from 'firebase/firestore'
 
 
 import {useState, useEffect} from 'react'
+
+
 
 function Popup({handlePopup}) {
     const [books, setBooks] = useState(null)
 
 
+    const addToLog = async id => {
+
+        const docRef = collection(db, 'sales')
+        console.log(id)
+        const targetBk = books.find(bk => bk.id === id)
+        const targetTitle = targetBk.title
+        const targetTotal = ((targetBk.price * targetBk.tax) + targetBk.price).toFixed(2)
+
+        const loc = navigator.language;
+
+        console.log(loc)
+    
+        await addDoc(docRef, {
+            title: targetTitle,
+            total: targetTotal,
+            time: new Date().toLocaleString('en-US')
+            })
+
+    }
+
     const handleSubtract = (id) => {
 
         // now, update the quantity of this document
         const docRef = doc(db, 'books', `${id}`)
+        console.log(docRef)
         updateDoc(docRef, {
             quantity: increment(-1)
         })
-        .then(() => console.log('quantity decremented'))
-
+        .then(() => addToLog(id))
+        
+        
         // Later, add this document to the Sales Log collection
 
         // .then(() => {
@@ -52,17 +76,17 @@ function Popup({handlePopup}) {
                         <th>Item #</th>
                         <th>Book Title</th>
                         <th>Qty in Stock</th>
-                        <th>Price</th>
+                        <th>Total Price</th>
                     </tr>
                 </thead>
                 <tbody>
                     {books && books.map((bk, index) => {
                         return (
-                            <tr key={index} style={{ backgroundColor: bk.quantity <= 1 ? '#f7bdbd' : bk.quantity <= 3 ? '#eae995' : '#b4f5b8'  }}>
+                            <tr key={index} style={{ backgroundColor: bk.quantity <= 1 ? '#f7bdbd' : bk.quantity <= 3 ? '#eae995' : '#b4f5b8' }}>
                                 <td>{bk.id}</td>
                                 <td>{bk.title}</td>
                                 <td className='qty'><span>{bk.quantity}</span><button onClick={() => handleSubtract(bk.id)}>subtract 1</button></td>
-                                <td>${bk.price}</td>
+                                <td>${((bk.price * bk.tax) + bk.price).toFixed(2)}</td>
                             </tr>
                         )})}
                 </tbody>
--- src/components/SalesLog.js
+++ src/components/SalesLog.js
@@ -2,17 +2,30 @@
 
 import {useState, useEffect} from 'react'
 
+// firebase imports
+import {db} from '../firebase/config'
+import {collection, getDocs} from 'firebase/firestore'
+
 
 
 function SalesLog({handleSalesLog}) {
+    const [salesLog, setSalesLog] = useState([]);
 
 
-
-
-    // retrieve any data stored in "sales_log" collection
+    // retrieve any data stored in "sales" collection
   useEffect(() => {
-    console.log("I fired")
-  })
+    const ref = collection(db, 'sales')
+    // const q = query(ref, orderBy("order", "asc"))
+    getDocs(ref)
+      .then((snapshot) => {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({id: doc.id, ...doc.data()})
+        })
+        setSalesLog(results)
+      })
+  }, [])
+
 
   return (
     <div className="popup-overlay" onClick={handleSalesLog}>
@@ -22,13 +35,18 @@ function SalesLog({handleSalesLog}) {
                 <thead>
                     <tr>
                         <th>Item Name</th>
+                        <th>Total Price</th>
                         <th>Purchase Date</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        Feature coming soon...
-                    </tr>
+                        {salesLog && salesLog.map(item => (
+                            <tr key={item.id} style={{ backgroundColor: '#b4f5b8' }}>
+                                <td>{item.title}</td>
+                                <td>${item.total}</td>
+                                <td>{item.time}</td>
+                            </tr>
+                        ))}
                 </tbody>
             </table>
         </div>
